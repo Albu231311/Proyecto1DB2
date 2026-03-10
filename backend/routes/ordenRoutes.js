9351e1fc5dcb286861c6468e9a74e7bdf72788d5
Camila Richter <ric23183@uvg.edu.gt>
2026-03-10 17:27:57 -0600

    feat: updated ordenRoutes

--- backend/routes/ordenRoutes.js
+++ backend/routes/ordenRoutes.js
@@ -2,21 +2,17 @@ const express = require('express');
 const router = express.Router();
 const ordenCtrl = require('../controllers/ordenController');
 
-// 1. Operaciones de Reportes 
+// 1. Reportes
 router.get('/reporte/detallado', ordenCtrl.getOrdenesDetalladas); 
 router.get('/reporte/totales', ordenCtrl.getTotalVentasPorRestaurante);
+router.get('/reporte/platillos-top', ordenCtrl.getPlatillosMasVendidos);
+router.get('/reporte/clientes-recurrentes', ordenCtrl.getClientesRecurrentes);
 
-// 2. Operaciones CRUD Estándar
+// 2. CRUD
 router.post('/', ordenCtrl.crearOrden);
 router.get('/', ordenCtrl.getOrdenes);
 router.get('/:id', ordenCtrl.getOrdenById);
 router.put('/:id', ordenCtrl.updateOrden);
 router.delete('/:id', ordenCtrl.deleteOrden);
-
-//platillos más vendidos
-router.get('/reporte/platillos-top', ordenCtrl.getPlatillosMasVendidos);
-
-// Ruta para el reporte de clientes top
-router.get('/reporte/clientes-recurrentes', ordenCtrl.getClientesRecurrentes);
 
 module.exports = router;
