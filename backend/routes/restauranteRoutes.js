9b396edccf7a972e8031a66d1d7e9ebdd338ea7d
Camila Richter <ric23183@uvg.edu.gt>
2026-03-10 17:28:49 -0600

    feat: updated restauranteRoutes

--- backend/routes/restauranteRoutes.js
+++ backend/routes/restauranteRoutes.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const restCtrl = require('../controllers/restauranteController'); // <--- Usamos restCtrl
+const restCtrl = require('../controllers/restauranteController');
 
-// 1. Rutas fijas 
+
+router.get('/buscar', restCtrl.buscarRestaurantes); 
 router.get('/top', restCtrl.getMejoresRestaurantes);
 router.get('/cercanos', restCtrl.getRestaurantesCercanos);
-
 router.get('/reporte/mejor-calificados', restCtrl.getMejorCalificados); 
 
-// 2. Rutas generales
+
 router.get('/', restCtrl.getRestaurantes);
 router.post('/', restCtrl.createRestaurante);
 
-// 3. Rutas con parámetros 
+
 router.get('/:id', restCtrl.getRestauranteById); 
 router.put('/:id', restCtrl.updateRestaurante);
 router.delete('/:id', restCtrl.deleteRestaurante);
